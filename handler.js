75b4c98f38d5024c25f4a6585980773e86d7c10b
Evan Zlotnick <evan_zlotnick@yahoo.com>
2019-04-26 14:08:00 -0700

    made changes to the policy to allow for wildcarding to make sure that users can access other functions using the same authorizer and wildcarding parts of the policy

--- handler.js
+++ handler.js
@@ -1,7 +1,16 @@
 
 
 module.exports.derp = async (event, context) => {
-  console.log(event.requestContext.authorizer); // This has the value of the user which was put in there by the authorizer
+  if (event.requestContext.authorizer.user) {
+      const user = JSON.parse(event.requestContext.authorizer.user);
+      console.log("The user calling the API is:");
+      console.log(user);
+      // Remember, by the definition in the serverless.yml the user will be cached up to 1 hour, therefore, if you make
+      // a change to a user, it might not take effect for an hour. If this is an issue, set the timeout to 5 minutes or lower
+  }
+  /*
+  Code to handle authorization of the authenticated user goes here, if the user should not access the API, return a status code 401
+   */
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -19,11 +28,12 @@ const generatePolicy = (effect, resource) => {
     let statementOne = {};
     statementOne.Action = 'execute-api:Invoke';
     statementOne.Effect = effect;
-    statementOne.Resource = resource;
+    const wildcardPath = resource.split(':');
+    statementOne.Resource = `${wildcardPath[0]}:${wildcardPath[1]}:${wildcardPath[2]}:${wildcardPath[3]}:*/*/*/*`;
     policyDocument.Statement[0] = statementOne;
   }
   response.policyDocument = policyDocument;
-  const user = {lastName: "Ever", firstName: "Greatest", stuff: "something"};
+  const user = {user: JSON.stringify({lastName: "Ever", firstName: "Greatest", stuff: "something"})};
   response.context = user;
   return response;
 };
@@ -31,7 +41,14 @@ const generatePolicy = (effect, resource) => {
 module.exports.authorizer = (event, context, callback) => { // NOTE: The authorizer function CANNOT do async yet
   console.log(`Client token: ${event.authorizationToken}`);
   console.log(`Method ARN: ${event.methodArn}`);
+  //  0 :      1        :   2  :     3      :           4
+  // arn:aws:execute-api:region:AWS ACCOUNT:APIGATEWAYRESTAPI/STAGE/VERB/FUNCTION
+  // If you use 1 AWS account and multiple stages, probably don't want to wildcard the stage portion of the ARN for security purposes!
+  // If there are multiple functions, remember the authorizer caches the response, so it will probably make sense
+  // to wildcard some of the policy document that way the same authorizer can be used for different functions
+  // let the authorizer by the authenticator that passes back a user, let the function then authorize based on the user and/or the user's roles
   if (event.authorizationToken === "derp") {
+    // Ideally, lookup a user session from the authorizationToken or call callback('Unauthorized') if the session cannot be found
     const policy = generatePolicy("Allow", event.methodArn);
     console.log("SUCCESS, generating policy:");
     console.log(JSON.stringify(policy));
